8ebf42fa5729fd369ad55db59c02af73d6cfcf5b
Anthony Moss <asmoss93@gmail.com>
2019-03-05 17:58:17 -0500

    adds findNamesStartA function that finds all characters that names start with A

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,16 @@
 console.log(`There are ${characters.length} characters in the array.`);
+
+function findNamesStartA() {
+    let names = characters;
+    let namesArray = []
+    characters.forEach(function (chars) {
+        let fullName = (chars.name);
+        if (fullName.startsWith('A')) {
+            namesArray.push(fullName)
+        }
+    // console.log(namesArray.length)
+    })
+    console.log(namesArray.length)
+}
+findNamesStartA()
+
